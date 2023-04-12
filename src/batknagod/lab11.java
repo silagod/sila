0809cd9820cfdc0723d1c0bce9410ebc96c03f7e
Admin <Admin@DESKTOP-53KNEMD>
2023-04-12 15:12:17 +0800

    yu c hamaagui bicew

---
+++ src/batknagod/lab11.java
@@ -0,0 +1,18 @@
+package batknagod;
+
+public class lab11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    } catch (Exception e) {
+		      System.out.println("batkana bicew.");
+		    }
+		  }
+		}
+	
+
+
